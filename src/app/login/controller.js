e069b11a5f931545e24c3822d4b3a5b52fe9fd51
Stephen Bussey <steve.bussey@salesloft.com>
2016-05-14 12:02:14 -0400

    update sateillizer which wasn't working locally

--- src/app/login/controller.js
+++ src/app/login/controller.js
@@ -3,6 +3,8 @@ import ngInject from '../decorators/ng_inject';
 @ngInject("$auth", "User")
 export class LoginController {
   google() {
-    this.$auth.authenticate('google').then(() => this.User.getAndRedirect());
+    this.$auth.authenticate('google').then(() => {
+      this.User.getAndRedirect();
+    });
   }
 }

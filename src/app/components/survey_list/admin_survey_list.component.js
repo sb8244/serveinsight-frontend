c10574b8309ff84f4166fe3ddc9637857fcd08cc
Stephen Bussey <steve.bussey@salesloft.com>
2016-05-26 15:01:10 -0700

    remove one time surveys

--- src/app/components/survey_list/admin_survey_list.component.js
+++ src/app/components/survey_list/admin_survey_list.component.js
@@ -25,29 +25,6 @@ class Controller {
         creator: "Brian Culler"
       }
     ];
-
-    this.oneTimeSurveys = [
-      {
-        id: 3,
-        name: "End of Project Report",
-        users_in_scope: 4,
-        responses: 1,
-        created_at: moment().subtract(1, "week").toJSON(),
-        active: true,
-        recurring: false,
-        creator: "Steve Bussey"
-      },
-      {
-        id: 4,
-        name: "Quarterly TPS",
-        users_in_scope: 88,
-        responses: 67,
-        created_at: moment().subtract(5, "week").toJSON(),
-        active: true,
-        recurring: false,
-        creator: "Steve Bussey"
-      }
-    ];
   }
 }
 

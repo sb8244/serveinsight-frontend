73c087a1bc7acba366d8eba6d944499994ce3172
Stephen Bussey <steve.bussey@salesloft.com>
2016-06-20 00:14:59 -0400

    correct auth and api base

--- secrets/production.js
+++ secrets/production.js
@@ -1,3 +1,3 @@
-export const authBase = 'http://serve.serveinsight.com/';
-export const apiBase = 'http://serve.serveinsight.com/api';
+export const authBase = '//serve.serveinsight.com/';
+export const apiBase = '//serve.serveinsight.com/api';
 export const googleClientID = '853844444042-dr4mg2qv7opci957nub40sktgf6ervhn.apps.googleusercontent.com';

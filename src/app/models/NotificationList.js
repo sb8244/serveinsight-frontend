1a9ec8d4d37203ba2b0fc35caf7e5660bc3183c0
Stephen Bussey <steve.bussey@salesloft.com>
2016-06-26 22:56:27 -0400

    more notification types

--- src/app/models/NotificationList.js
+++ src/app/models/NotificationList.js
@@ -13,11 +13,21 @@ class Notification {
     _.assign(this, data);
   }
 
+  /*
+   * Passups, reviews, answer comments, goal comments, instance comments, answer mention, goal mention, comment mention
+   */
   uiSref() {
+    var detailType = this.notification_details.commentable_type || this.notification_details.mentionable_type;
+    var detailId = this.notification_details.commentable_id || this.notification_details.mentionable_id;
+
     if (this.notification_type === "passup") {
       return `passups.show({ id: ${this.notification_details.passup_id} })`;
     } else if (this.notification_type === "review") {
       return `surveys.reports_show({ id: ${this.notification_details.survey_instance_id} })`;
+    } else if (detailType === "Answer") {
+      return `answers_show({ id: ${detailId} })`;
+    } else if (detailType === "SurveyInstance") {
+      return `surveys.completed_show({ id: ${detailId} })`;
     }
 
     return "dashboard";

68ed2bcc1080f62d12acb1cb85150a6b4dc97537
Stephen Bussey <steve.bussey@salesloft.com>
2016-08-05 19:18:36 -0400

    Not invite only

--- src/app/routes/login/login.controller.js
+++ src/app/routes/login/login.controller.js
@@ -3,6 +3,6 @@ import ngInject from '../../decorators/ng_inject';
 @ngInject("$stateParams")
 export class LoginController {
   allowed() {
-    return !!this.$stateParams.inviteOnly;
+    return true;
   }
 }
